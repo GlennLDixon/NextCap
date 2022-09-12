fddd718292065af41b2bae5bae4ab3bba37a4aad
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-12 16:19:53 -0500

    added the authenciation view

--- nextapi/views/auth.py
+++ nextapi/views/auth.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
